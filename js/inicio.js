c16c97dd52951373996999f2675aae7b8b34477e
Francisco Rendo <dm,n f;kxdsf.me>
2020-11-19 20:34:08 -0300

    api en el inicio funcionando trae las series pero desordenadas

--- js/inicio.js
+++ js/inicio.js
@@ -7,6 +7,20 @@ fetch("https://api.themoviedb.org/3/movie/popular?api_key=704bd3935947752adbb2e6
     console.log(peliculas);
 
     for (var i = 0; i < peliculas.length; i++) {
-      document.querySelector(".posters").innerHTML += '<a class="mobile" ><img src="https://image.tmdb.org/t/p/original/' + peliculas[i].poster_path + '" alt=""></a>'
+      document.querySelector(".posters").innerHTML += '<a class="mobile " ><img src="https://image.tmdb.org/t/p/original/' + peliculas[i].poster_path + '" alt=""></a>'
+    }
+  })
+
+
+  fetch("https://api.themoviedb.org/3/trending/tv/week?api_key=239cb21a3c20983f2c69678890e9289a")
+  .then(function(respuesta) {
+    return respuesta.json()
+  })
+  .then(function(datos) {
+    var series = datos.results
+    console.log(series);
+
+    for (var i = 0; i < series.length; i++) {
+      document.querySelector(".series").innerHTML += '<a class="mobile " ><img src="https://image.tmdb.org/t/p/original/' + series[i].poster_path + '" alt=""></a>'
     }
   })
