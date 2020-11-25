5340f1e6da313d3c9761b96d6596a5e114fc7e7c
martingotz <gotzm@udesa.edu.ar>
2020-11-25 17:51:42 -0300

    a

---
+++ js/PeliculasXGenero.js
@@ -0,0 +1,20 @@
+window.addEventListener('load', function(){
+    var querystring = location.search
+    var url = new URLSearchParams(querystring)
+    var id = url.get('id')
+    var name = url.get ('name')
+    console.log(id);
+    console.log(name)
+fetch (`https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69678890e9289a&with_gender=${id}`)
+.then(function(respuesta) {
+    return respuesta.json()
+  })
+      .then(function(datos) {
+    var PeliculasXGenero = datos.results
+    console.log(PeliculasXGenero);
+
+       for (var i = 0; i < PeliculasXGenero.length; i++) {
+      document.querySelector(".generoPeliculas").innerHTML += '<a class="mobile " ><img src="https://image.tmdb.org/t/p/original/' + PeliculasXGenero[i].poster_path + '" alt=""></a>'
+    }
+  })
+})
--- js/genero.js
+++ js/genero.js
@@ -1,64 +1,16 @@
 window.addEventListener('load',function(){
-
+var generoPeliculas = document.querySelector ('.generoPeliculas')
 
   fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=239cb21a3c20983f2c69678890e9289a&language=es-Es')
   .then(function(respuesta) {
     return respuesta.json()
     })
   .then(function(peliculasG) {
+
     peliculasG.genres.forEach(genero => {
-      nombreGenero.push(genero.name)
-      idGenero.push (genero.id)
-      console.log(idGenero);
-      console.log(nombreGenero);
-    })
-   
-  //let selectgeneroPeliculas =document.querySelector('#generoPeliculas')
-  /*function cargarGeneroPeliculas(){
-    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=239cb21a3c20983f2c69678890e9289a&language=es-Es')
-    .then(function(respuesta){
-      //console.log(respuesta.json())
-      return respuesta.json()
-    })
-    .then(function(datosGeneroPeliculas){
-      //console.log(datosProvincias.provincias)
-      //Aquí le incorporo elementos html desde javascript
-      selectgeneroPeliculas.innerHTML = "<option value = ''>Seleccione provincia... </option>"
-      for (let dato of datos.GeneroPeliculas.id){
-        //console.log(dato)
-        let generoPeliculas = document.createElement('option')
-        generoPeliculas.setAttribute('value',dato.id)
-        generoPeliculas.innerHTML = dato.nombre
-        selectgeneroPeliculas.append(generoPeliculas)
-      }
-    })
-  }*/
-
-  /*fetch("")
-    .then(function(respuesta) {
-      return respuesta.json()
-    })
-    .then(function(datos) {
-      var generoPeliculas = datos.results
-      console.log(generoPeliculas);
-  
-      for (var i = 0; i < 10; i++) {
-        document.querySelector(".generoPeliculas").innerHTML += `<a href='DetallePelicula.html?id=${generoPeliculas[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${generoPeliculas[i].poster_path}'   alt='${generoPeliculas[i].title}' /></a>`
-      }
       
-    })*/
-
-    /*fetch("https://api.themoviedb.org/3/discover/tv?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&sort_by=popularity.desc&page=1&timezone=America%2FNew_York&include_null_first_air_dates=false")
-    .then(function(respuesta) {
-      return respuesta.json()
+    generoPeliculas.innerHTML += `<li> <a href='PeliculasXGenero.html?id=${genero.id}&name=${genero.name}'    class='mobile ' > ${genero.name} </a>
+ </li>`
     })
-    .then(function(datos) {
-      var generoSeries = datos.results
-      console.log(generoSeries);
-  
-      for (var i = 0; i < 10; i++) {
-        document.querySelector(".generoSeries").innerHTML += `<a href='DetallePelicula.html?id=${generoSeries[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${generoSeries[i].poster_path}'   alt='${generoSeries[i].title}' /></a>`
-      }
-    })*/
-  
+    })
     })
