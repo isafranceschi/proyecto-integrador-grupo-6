5eea9c0b19019131cb8e7dbea5b2106bd96e593e
martingotz <gotzm@udesa.edu.ar>
2020-11-25 00:51:09 -0300

    00:50

--- js/generios.js
+++ js/generios.js
@@ -22,7 +22,41 @@ window.addEventListener('load',function(){
 
 
 
-
+        fetch ("https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+        .then(function(response){
+        return response.json();
+        })
+        .then(function(resultado){
+            console.log (resultado);
+            var listaDeGeneros = document.querySelector ("#generoPeliculas")
+            var contenidoDeGeneros =data.results;
+            
+           for(let i=0 ; i < 10 ; i++){
+           document.querySelector(".generoPeliculas").innerHTML += `<a href='DetallePelicula.html?id=${listaDeGeneros[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${contenidoDeGeneros[i].poster_path}'   alt='${contenidoDeGeneros[i].title}' /></a>`}
+ 
+           listaDeGeneros.innerHTML = contenidoDeGeneros;
+ 
+        })
+        .catch(function(error){
+            console.log("Error" + error);
+        })
+ 
+     fetch("https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+     .then(function(respuesta) {
+       return respuesta.json()
+     })
+     .then(function(datos) {
+       var masvaloradas2 = datos.results
+       var genre_ids = datos.results
+       var accion = genre_ids.filtr(function(genre_ids){
+           return accion=28
+       })
+       console.log(genre_ids);
+   
+       for (var i = 0; i < 10; i++) {
+         document.querySelector(".peliculas1").innerHTML += `<a href='DetallePelicula.html?id=${masvaloradas2[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${masvaloradas2[i].poster_path}'   alt='${masvaloradas2[i].title}' /></a>`
+       }
+     })
 
 
 
--- js/genero.js
+++ js/genero.js
@@ -1,49 +1,27 @@
 window.addEventListener('load',function(){
-    fetch ("https://api.themoviedb.org/3/genre/movie/list?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES")
-       .then(function(response){
-       return response.json();
-       })
-       .then(function(resultado){
-           console.log (resultado);
-           var listaDeGeneros = document.querySelector ("#generoPeliculas")
-           var contenidoDeGeneros =data.results;
-           
-          for(let i=0 ; i < 10 ; i++){
-          document.querySelector(".generoPeliculas").innerHTML += `<a href='DetallePelicula.html?id=${listaDeGeneros[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${contenidoDeGeneros[i].poster_path}'   alt='${contenidoDeGeneros[i].title}' /></a>`}
-
-          listaDeGeneros.innerHTML = contenidoDeGeneros;
-
-       })
-       .catch(function(error){
-           console.log("Error" + error);
-       })
-
-
-
-
-
-
-
-
-
-
-    fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&page=1")
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
     .then(function(respuesta) {
       return respuesta.json()
     })
     .then(function(datos) {
-      var masvaloradas2 = datos.results
-      var genre_ids = datos.results
-      var accion = genre_ids.filtr(function(genre_ids){
-          return accion=28
-      })
-      console.log(genre_ids);
+      var generoPeliculas = datos.results
+      console.log(generoPeliculas);
   
       for (var i = 0; i < 10; i++) {
-        document.querySelector(".peliculas1").innerHTML += `<a href='DetallePelicula.html?id=${masvaloradas2[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${masvaloradas2[i].poster_path}'   alt='${masvaloradas2[i].title}' /></a>`
+        document.querySelector(".generoPeliculas").innerHTML += `<a href='DetallePelicula.html?id=${generoPeliculas[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${generoPeliculas[i].poster_path}'   alt='${generoPeliculas[i].title}' /></a>`
       }
     })
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69678890e9289a&language=es-ES&sort_by=popularity.desc&include_adult=false&include_video=false&page=1")
+    .then(function(respuesta) {
+      return respuesta.json()
+    })
+    .then(function(datos) {
+      var generoSeries = datos.results
+      console.log(generoSeries);
   
-
+      for (var i = 0; i < 10; i++) {
+        document.querySelector(".generoSeries").innerHTML += `<a href='DetallePelicula.html?id=${generoSeries[i].genre_ids}'   class='mobile ' ><img src='https://image.tmdb.org/t/p/w500${generoSeries[i].poster_path}'   alt='${generoSeries[i].title}' /></a>`
+      }
+    })
   
     })
