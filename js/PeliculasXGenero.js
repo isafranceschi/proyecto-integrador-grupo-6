9db35fa5e6347c7ff9ce8172e4c8e03f10be4c4d
martingotz <gotzm@udesa.edu.ar>
2020-11-27 10:03:56 -0300

    actualizacion

--- js/PeliculasXGenero.js
+++ js/PeliculasXGenero.js
@@ -15,6 +15,7 @@ fetch (`https://api.themoviedb.org/3/discover/movie?api_key=239cb21a3c20983f2c69
 
        for (var i = 0; i < PeliculasXGenero.length; i++) {
       document.querySelector(".PeliculasXGenero").innerHTML += `<a href='DetallePelicula.html?id=${PeliculasXGenero[i].id}' class='mobile ' ><img src='https://image.tmdb.org/t/p/original/`+PeliculasXGenero[i].poster_path + `' alt='${PeliculasXGenero[i].title}'></a>`
+      
     }
   })
  
--- js/miLista.js
+++ js/miLista.js
@@ -1,9 +1,9 @@
 window.addEventListener('load', function(){
     let miListaDeFavoritas =  JSON.parse(localStorage.getItem('miLista'))
-    //console.log(miListaDeFavoritas);
-    let peliculas = document.querySelector('.peliculas')
+    console.log(miListaDeFavoritas);
+    //let peliculas = document.querySelector('.')
     for(let i = 0; i < miListaDeFavoritas.length; i++){
-        //console.log(populares.results[i].poster_path);
+        console.log(populares.results[i].poster_path);
         let articulo = document.createElement('article')
         articulo.classList.add('articulo')
         articulo.innerHTML += `<a href='detallePelicula.html?id=${miListaDeFavoritas[i].id}&titulo=${miListaDeFavoritas[i].title}'>  <img src='https://image.tmdb.org/t/p/w500${miListaDeFavoritas[i].poster_path}' alt = '${miListaDeFavoritas[i].title}' /> </a> 
@@ -13,7 +13,28 @@ window.addEventListener('load', function(){
         `
         peliculas.append(articulo)
     }
+    let botonMiLista = document.querySelectorAll('.botonMiLsta')
+    let arrayMiListaDeFavoritas;
+    console.log(botonMiLista)
+    botonMiLista.forEach(pelicula => {
+        pelicula.addEventListener('click', function(e){
+            e.preventDefault()
+            let miListadePeliculas = localStorage.getItem('miLista')
+            if(miListadePeliculas == null){
+                arrayMiListaDeFavoritas = [];
+            }else{
+                arrayMiListaDeFavoritas = JSON.parse(miListadePeliculas)
+            }
+            //Si no les funciona de esta forma
+            //-------------------------------------------------
+            //arrayMiListaDeFavoritas.push(JSON.parse(this.id))
+            //--------------------------------------------------
+            //Pueden utilizar esta otra forma de atrapar el elemento
+            //-----------------------------------------------------
+            arrayMiListaDeFavoritas.push(JSON.parse(e.target.href))
+            //-----------------------------------------------------    
+            localStorage.setItem('miLista', JSON.stringify(arrayMiListaDeFavoritas))
+        } )
+    });
 
-
-})
 })
